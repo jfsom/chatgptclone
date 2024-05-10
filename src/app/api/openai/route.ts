8ad0bf31b8fe5e77c36cfd8e1eeffc51de4675b6
jfsom <vijay.jfsom@gmail.com>
2024-05-10 09:10:25 +0530

    First commit

---
+++ src/app/api/openai/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from 'next/server';
+import OpenAI from 'openai';
+
+const openai = new OpenAI({ apiKey: process.env['OPENAI_API_KEY'] });
+// import { Configuration, OpenAIApi } from 'openai';
+
+// const configuration = new Configuration({
+//     apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
+
+export async function POST(request: NextRequest) {
+  // Check if the request has a valid JSON body
+  const contentType = request.headers.get('content-type');
+  if (!contentType || !contentType.includes('application/json')) {
+    return new NextResponse('Invalid content type', { status: 400 });
+  }
+
+  try {
+    // Parse the JSON body to extract the 'query' property
+    const requestBody = await request.json();
+    const { query } = requestBody;
+
+    const chat_completion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: query }],
+    });    
+
+    // return NextResponse.json({ response: chat_completion.data.choices[0].message.content });
+
+    return NextResponse.json({ response: chat_completion.choices[0].message.content });
+
+  } catch (error) {
+    return new NextResponse('Invalid JSON data', { status: 400 });
+  }
+}
